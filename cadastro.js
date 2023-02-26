2f413f1e83e2b2d150251724697f76dfcd0b1c71
Quintino-tec <filhoquintino4@gmail.com>
2023-02-26 02:29:05 -0300

    Adicionando funcionalidade de mostrar mensagem

--- cadastro.js
+++ cadastro.js
@@ -2,4 +2,11 @@ function signUp(email){
     return email;
 }
 
+
 //pegando o email e cadastrando na plataforma.
+
+function mostraMensagem() {
+    return 'Bem vindo ao sistema';
+}
+
+//Adicionando função de mensagem
